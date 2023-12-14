8b1ab70d99fa97de8e83cffee6b430536c0b079f
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-12-14 12:23:30 +1000

    fuxed db connection bug, to do with uuid

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sys
 from input_data import DataManager
 
 class Network():
+    # TODO: SQLite INSERT statements always return -1 rowcount, so we cant use this param for error checking. use another approach,
+    # preferably without extra calls to db
     def __init__(self, layer_count:int, layer_size:int, input_size:int, output_size:int, activation_type:int, learning_rate:float) -> None:
         self.layer_size=layer_size
         self.input_size=input_size
@@ -12,14 +14,25 @@ class Network():
         self.layer_count=layer_count
         self.learning_rate=learning_rate
         self.cum_error:float=0.00
-        self.conn=sqlite3.Connection(f'hidden-layers-{uuid.uuid4().hex[:8]}')
+        self.session_string:str=uuid.uuid4().hex[:8]
+        print(f"INSTANCING DB;/.{self.session_string}")
+        self.conn=sqlite3.Connection(f'hidden-layers-{self.session_string}.db')
         self.cursor=self.conn.cursor()
         self.run_sql_file('Create.sql')
         is_first_layer:bool=True
         for layer in range(0,layer_count):
             self.cursor.execute(f'INSERT INTO layer VALUES(?,?)', (layer,activation_type))
-            for node in range(0,layer_size):
+            print("ADDING layer")
+            layer_size_for_layer:int
+            if layer==0:
+                layer_size_for_layer=input_size
+            else:
+                layer_size_for_layer=layer_size
+            for node in range(0,layer_size_for_layer):
                 self.cursor.execute(f'INSERT INTO node VALUES(?,?)', (layer,node))
+                print("ADDING node")
+                if self.cursor.rowcount==0:
+                    raise Exception
                 if is_first_layer==True:
                     layer_input_size:int=input_size
                 else:
@@ -27,11 +40,17 @@ class Network():
                 for fromNode in range(0,layer_input_size):
                     weight:float=random.uniform(-1.000, 1.000)
                     self.cursor.execute(f'INSERT INTO weights VALUES(?,?,?,?)',(node,fromNode,layer,weight))
+                    print("ADDING weight")
+                    self.conn.commit()
+                    if self.cursor.rowcount==0:
+                        raise Exception
                 is_first_layer=False
         for node in range(0, layer_size):
             for output in range(0, output_size):
                 weight:float=random.uniform(-1.000, 1.000)
                 self.cursor.execute('INSERT INTO weights VALUES(?,?,?,?)', (output, node, layer_count, weight))
+                if self.cursor.rowcount==0:
+                    raise Exception
 
     def run_sql_file(self, file:str):
         path:str=f"sql/{file}"
@@ -56,6 +75,7 @@ class Network():
     # I have intentionally left this un-typed, so it will work for any numerical value. Error handling should be implemented here.
     @staticmethod
     def normalize_list(values:[])->[]:
+        #print(f'\t{values}')
         if not values:
             return []
         value_min = min(values) 
@@ -73,7 +93,7 @@ class Network():
 
     # Classification algorithm, reducing many floats to one int 
     def forward_propagate(self, input_data:list[float], expected_result:int, is_training:bool=True):
-        print(f"len: {len(input_data)}")
+        #print(f"len: {len(input_data)}")
         weights_used:list=[]
         current_layer:list[float]=[]
         layer_cache:list[float]
@@ -91,14 +111,14 @@ class Network():
             else:
                 layer_size=self.layer_size
                 layer_cache=current_layer
-            #print(1)
-            if(i!=len(input_data)-1):
-                output_size:int=self.layer_size
+            #print('\t', k, self.layer_count-1)
+            if(k!=self.layer_count-1):
+                output_size:int=self.output_size
                 #print(i, len(input_data))
-                #print("VERY LAST VERY LAST VERY LAST VERY LAST VERY LAST VERY LAST VERY LAST VERY LAST ")
+                print("VERY LAST VERY LAST VERY LAST VERY LAST VERY LAST VERY LAST VERY LAST VERY LAST ")
             else:
                 #print(f"{i} is not equal to {output_size}")
-                output_size=self.output_size
+                output_size=self.layer_size
             i=0
             # For every node in this layer
             while i < layer_size:
@@ -107,10 +127,10 @@ class Network():
 
                 #print(f'\t\t{self.output_size}')
                 # For all possible outputs
+                #print(output_size)
                 while(j<output_size):
                     # get weight for this input-output combo
                     self.cursor.execute('SELECT weight, fromNodeId, toNodeId, layerId FROM weights WHERE fromNodeId=? AND toNodeId=?', (i,j))
-                    #print(i,j)
                     weight, fromNodeId, toNodeId, layerId=self.cursor.fetchone()
                     # add input by weight to this node
                     adjustment:float=layer_cache[i]*weight
@@ -123,6 +143,7 @@ class Network():
             current_layer=self.normalize_list(current_layer)
             for layer_index in range(0,len(current_layer)):
                 current_layer[layer_index]=self.activation_relu(current_layer[layer_index])
+            layer_cache=current_layer
             k+=1
         result:int=current_layer.index(max(current_layer))
         error=self.calculate_error(result, expected_result)
@@ -135,7 +156,7 @@ class NetworkManager():
     def __init__(self) -> None:
         self.data:DataManager=DataManager()
         self.nn:Network=Network(
-            layer_count=1, 
+            layer_count=3, 
             layer_size=64, 
             input_size=784, 
             output_size=10, 
@@ -154,4 +175,5 @@ class NetworkManager():
             test_point:{}=self.data.get_random_training_data_point()
             self.nn.forward_propagate(test_point['inputs'],int(test_point['label']))
 
-NetworkManager().train(20000)
+ml=NetworkManager()
+ml.train(20000)
