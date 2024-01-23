corrected iteration count not resetting. model works - experiencing exploding gradients

02_cnn_np_ops/main.py
@@ -32,34 +32,76 @@ for i in range(0, c):
     if i%2==0:
         weights.append(np.random.uniform(-1.000, 1.000, size=(model[i].output.shape[1], model[i+1].output.shape[1])))
 
+# NOTE: This loop has been set up to feed in one training example repeatedly. This
+# is not typically useful, but serves to prove that the model will approach truth.
+# Because the model averages saved outputs across the forward-iteration count,
+# it can be fed batches, and will automatically average and clear saved outputs as
+# needed (see ops.p)
 # forward prop
-feature:np.ndarray=np.array([[0.1, 0.8]])
-expected:np.ndarray=np.array([[0, 0.9, 0]])
-a=model[0].forward_propagate({},feature)
-b=model[1].forward_propagate({}, weights[0], a, b1)
-c=model[2].forward_propagate({}, b)
-d=model[3].forward_propagate({}, weights[1], c, b2)
-e=model[4].forward_propagate({}, d)
-Loss=model[5].forward_propagate({}, e, expected)
-# NOTE: The following (commented-out) line of code shows the forward pass as a 
-# single nested function. This is identical to the above approach in practice. 
-# Loss=f(x)=0.5(sig(xW+b))^2
-#Loss=model[5].forward_propagate({}, model[4].forward_propagate({}, model[3].forward_propagate({}, weights[1], model[2].forward_propagate({}, model[1].forward_propagate({}, weights[0], model[0].forward_propagate({},feature), 0)), 0)), expected)
-print(f"MSE Loss: {Loss}\n")
+for i in range(0,100):
+    feature:np.ndarray=np.array([[0.1, 0.8]])
+    expected:np.ndarray=np.array([[0, 0.9, 0]])
+    a=model[0].forward_propagate({},feature)
+    b=model[1].forward_propagate({}, weights[0], a, b1)
+    c=model[2].forward_propagate({}, b)
+    d=model[3].forward_propagate({}, weights[1], c, b2)
+    e=model[4].forward_propagate({}, d)
+    Loss=model[5].forward_propagate({}, e, expected)
+    # NOTE: The following (commented-out) line of code shows the forward pass as a 
+    # single nested function. This is identical to the above approach in practice. 
+    # Loss=f(x)=0.5(sig(xW+b))^2
+    #Loss=model[5].forward_propagate({}, model[4].forward_propagate({}, model[3].forward_propagate({}, weights[1], model[2].forward_propagate({}, model[1].forward_propagate({}, weights[0], model[0].forward_propagate({},feature), 0)), 0)), expected)
+    print(f"MSE Loss: {Loss}\n")
 
-# backward prop. 
-# 'bp' just stands for back-propagated. This is the vector of dk^i/dL^i. 
-# Saving this value before computing nabla's not only saves a duplicate 
-# computation, but actually saves ^2 computations per layer added to the 
-# computation graph.
-bp=model[5].backward_propagate()*model[4].backward_propagate()
-b_nabla=sum(bp)
-w_nabla=np.outer(bp, model[2].output).T
-weights[1]-=alpha*w_nabla
-b2-=alpha*b_nabla
+    # backward prop. 
+    # 'bp' just stands for back-propagated. This is the vector of dk^i/dL^i. 
+    # Saving this value before computing nabla's not only saves a duplicate 
+    # computation, but actually saves ^2 computations per layer added to the 
+    # computation graph.
+    bp=model[5].backward_propagate()*model[4].backward_propagate()
+    b2_nabla=np.sum(bp)
+    w2_nabla=np.outer(bp, model[2].output).T
 
-bp=bp.dot(weights[1].T)*model[2].backward_propagate()
-b_nabla=sum(bp)
-w_nabla=np.outer(bp, model[0].output).T
-weights[0]-=alpha*w_nabla
-b1-=b_nabla
+    bp=bp.dot(weights[1].T)*model[2].backward_propagate()
+    b_nabla=np.sum(bp)
+    w_nabla=np.outer(bp, model[0].output).T
+
+    # apply nabla's
+    weights[0]-=alpha*w_nabla
+    b1-=alpha*b_nabla
+    weights[1]-=alpha*w2_nabla
+    b2-=alpha*b2_nabla
+'''for i in range(0,100000):
+    # forward prop
+    feature:np.ndarray=np.array([[0.1, 0.8]])
+    expected:np.ndarray=np.array([[0, 0.9, 0]])
+    a=model[0].forward_propagate({},feature)
+    b=model[1].forward_propagate({}, weights[0], a, b1)
+    c=model[2].forward_propagate({}, b)
+    d=model[3].forward_propagate({}, weights[1], c, b2)
+    e=model[4].forward_propagate({}, d)
+    Loss=model[5].forward_propagate({}, e, expected)
+    # NOTE: The following (commented-out) line of code shows the forward pass as a 
+    # single nested function. This is identical to the above approach in practice. 
+    # Loss=f(x)=0.5(sig(xW+b))^2
+    #Loss=model[5].forward_propagate({}, model[4].forward_propagate({}, model[3].forward_propagate({}, weights[1], model[2].forward_propagate({}, model[1].forward_propagate({}, weights[0], model[0].forward_propagate({},feature), 0)), 0)), expected)
+    if(i%50==0):
+        print(f"MSE Loss: {Loss}\n")
+
+    # backward prop. 
+    # 'bp' just stands for back-propagated. This is the vector of dk^i/dL^i. 
+    # Saving this value before computing nabla's not only saves a duplicate 
+    # computation, but actually saves ^2 computations per layer added to the 
+    # computation graph.
+    bp=model[5].backward_propagate()*model[4].backward_propagate()
+    b_nabla=sum(bp)
+    print(model[0].output)
+    w_nabla=np.outer(bp, model[2].output).T
+    weights[1]-=alpha*w_nabla
+    b2-=alpha*b_nabla
+
+    bp=bp.dot(weights[1].T)*model[2].backward_propagate()
+    b_nabla=sum(bp)
+    w_nabla=np.outer(bp, model[0].output).T
+    weights[0]-=alpha*w_nabla
+    b1-=b_nabla'''

02_cnn_np_ops/ops.py
@@ -28,6 +28,10 @@ class SigOp(Op):
         else:
             self.k+=inputs
         def sigmoid_outer(power:float):
+            if power > 0:
+                power=min(power, 700)
+            else:
+                power=max(-700, power)
             return 1/(1+(math.e**(-power)))
         inputs=np.vectorize(sigmoid_outer)(inputs)
         self.output+=inputs
@@ -37,6 +41,7 @@ class SigOp(Op):
         self.k=self.k/self.batch_size_count
         forward=self.forward_propagate(context, self.k)
         self.output=np.zeros(shape=self.output.shape)
+        self.batch_size_count=0
         return forward*(1-forward)
 
 class SumOp(Op):
@@ -54,7 +59,7 @@ class SumOp(Op):
     def forward_propagate(self, context:{}, weights:np.ndarray, x:np.ndarray, bias:float):
         self.increment_count()
         output=x.dot(weights)+bias
-        if(self.weights==None):
+        if(self.weights is None):
             self.weights=weights
         else:
             self.weights+=weights
@@ -64,6 +69,7 @@ class SumOp(Op):
     def backward_propagate(self):
         bp=self.weights.T
         self.output=np.zeros(shape=self.output.shape)
+        self.batch_size_count=0
         return bp
         
 
@@ -76,4 +82,5 @@ class MSEOp(Op):
     def backward_propagate(self):
         bp=np.sum(self.output)
         self.output=np.zeros(shape=self.output.shape)
+        self.batch_size_count=0
         return bp